1701ff6716bd52714a1a030d6936588d5ef8496f
Sheik Hazrin Bin Sheik Othman <sheikhazrin@outlook.my>
2020-11-23 21:28:42 +0800

    Auth0: phase 1 codesandbox

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+const element = (
+  <Auth0Provider
+    domain="careetor.us.auth0.com"
+    clientId="xRnWO6IOXqUhZUvF1ptofy6dN660oler"
+    redirectUri="{window.location.origin}"
+  >
+    <App />
+  </Auth0Provider>
+);
+ReactDOM.render(element, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

d1d65a2755d944daa41ef67d3f0f641296310934
Sheik Hazrin Bin Sheik Othman <sheikhazrin@outlook.my>
2020-11-24 10:39:46 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React from "react";
+import React from 'react';
 // import { useEffect, useState } from "react";
-import { Router } from "react-router-dom";
+import { Router } from 'react-router-dom';
 // import { Container } from "reactstrap";
-import theme from "./themes";
-import Routes from "./routes";
+import theme from './themes';
+import Routes from './routes';
 
-import { useAuth0 } from "@auth0/auth0-react";
-import history from "./services/History";
-import Loading from "./components/Loading";
+import { useAuth0 } from '@auth0/auth0-react';
+import history from './services/History';
+import Loading from './components/Loading';
 
-import { ThemeProvider } from "@material-ui/core/styles";
-import { Paper, CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Paper, CssBaseline } from '@material-ui/core';
 
 // import "./App.css";
 // import GlobalStyles from "./styles/";
 
 function App() {
-  const { isLoading, error } = useAuth0();
-  if (isLoading) {
-    return <Loading />;
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-  return (
-    <div id="app" className="Container">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Paper>
-          <Router history={history}>
-            <Routes />
-          </Router>
-        </Paper>
-      </ThemeProvider>
-    </div>
-  );
+	const { isLoading, error } = useAuth0();
+	if (isLoading) {
+		return <Loading />;
+	}
+	if (error) {
+		return <div>Oops... {error.message}</div>;
+	}
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Paper>
+				<Router history={history}>
+					<div id="app" className="Container">
+						{' '}
+					</div>
+					<Routes />
+				</Router>
+			</Paper>
+		</ThemeProvider>
+	);
 }
 export default App;
